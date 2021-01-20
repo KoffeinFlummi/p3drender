ec26e205b70d52110b5377f116519fadb8907ac1
KoffeinFlummi <koffeinflummi@protonmail.com>
2021-01-20 22:00:24 +0100

    Update for Blender 2.8+

--- p3drender/renderscript.py
+++ p3drender/renderscript.py
@@ -12,27 +12,22 @@ nan = 0.0 #don't ask...
 
 {}
 
-def create_texture(texturepath):
-    tex = bpy.data.textures.new(texturepath, type = 'IMAGE')
-    try:
-        tex.image = bpy.data.images.load(texturepath)
-    except:
-        pass
-    return tex
-
-def create_material(texture):
+def create_material(path):
     mat = bpy.data.materials.new("blub")
-    mat.specular_hardness = 200
+    mat.roughness = 0.8
     mat.specular_intensity = 0.02
 
-    mtex = mat.texture_slots.add()
-    mtex.texture = texture
-    mtex.texture_coords = 'UV'
-    mtex.use_map_color_diffuse = True
-    mtex.use_map_color_emission = True
-    mtex.emission_color_factor = 0.5
-    mtex.use_map_density = True
-    mtex.mapping = 'FLAT'
+    mat.use_nodes = True
+    tex = mat.node_tree.nodes.new('ShaderNodeTexImage')
+    try:
+        tex.image = bpy.data.images.load(path)
+    except Exception as e:
+        print(e)
+        pass
+
+    bsdf = mat.node_tree.nodes["Principled BSDF"]
+    mat.node_tree.links.new(bsdf.inputs['Base Color'], tex.outputs['Color'])
+
     return mat
 
 def create_mesh(verts, faces, uvs, normals, sharp_edges):
@@ -41,15 +36,10 @@ def create_mesh(verts, faces, uvs, normals, sharp_edges):
     ob.show_name = True
 
     scn = bpy.context.scene
-    scn.objects.link(ob)
-    scn.objects.active = ob
-    ob.select = True
+    scn.collection.objects.link(ob)
 
     me.from_pydata(verts, [], faces)
     me.update()
-
-    #me.normals_split_custom_set(functools.reduce(lambda x,y: x+y, normals))
-    #me.use_auto_smooth = True
 
     for f in me.polygons:
         f.use_smooth = True
@@ -59,24 +49,23 @@ def create_mesh(verts, faces, uvs, normals, sharp_edges):
         e.use_edge_sharp = vi in sharp_edges or vi[::-1] in sharp_edges
     ob.modifiers.new(name="Edge Split", type="EDGE_SPLIT")
 
-    bpy.ops.object.mode_set(mode='EDIT')
-    bm = bmesh.from_edit_mesh(me)
+    bm = bmesh.new()
+    bm.from_mesh(me)
 
     uv_layer = bm.loops.layers.uv.verify()
-    bm.faces.layers.tex.verify()
 
     for f, uvs in zip(bm.faces, UVS):
         for l, uv in zip(f.loops, uvs):
             l[uv_layer].uv = (uv[0], 1 - uv[1])
 
-    bmesh.update_edit_mesh(me)
-    bpy.ops.object.mode_set(mode='OBJECT')
+    bm.to_mesh(me)
+    bm.free()
 
     return ob
 
 def main():
-    bpy.data.objects.remove(bpy.data.objects["Cube"], True)
-    bpy.data.objects.remove(bpy.data.objects["Lamp"], True)
+    bpy.data.objects.remove(bpy.data.objects["Cube"], do_unlink=True)
+    bpy.data.objects.remove(bpy.data.objects["Light"], do_unlink=True)
 
     ob = create_mesh(VERTS, FACES, UVS, NORMALS, SHARP_EDGES)
     me = ob.data
@@ -94,8 +83,7 @@ def main():
         ob.location -= bbox_center
 
     # Apply materials
-    textures = [create_texture(x) for x in TEXTURES]
-    materials = [create_material(x) for x in textures]
+    materials = [create_material(x) for x in TEXTURES]
 
     for m in materials:
         me.materials.append(m)
@@ -104,13 +92,10 @@ def main():
         f.material_index = i
 
     # Prepare scene
-    lamp_data = bpy.data.lamps.new(name="New Lamp", type="HEMI")
+    lamp_data = bpy.data.lights.new(name="New Lamp", type="SUN")
     lamp_data.energy = 5
     lamp_object = bpy.data.objects.new(name="New Lamp", object_data=lamp_data)
-    bpy.context.scene.objects.link(lamp_object)
-
-    lamp_object.select = True
-    bpy.context.scene.objects.active = lamp_object
+    bpy.context.scene.collection.objects.link(lamp_object)
 
     # Prepare camera
     camera = bpy.context.scene.camera
@@ -120,8 +105,8 @@ def main():
 
     camera.rotation_euler = (math.radians(90) + CAMERA_PITCH, 0, -CAMERA_YAW)
 
-
-    bpy.context.scene.render.alpha_mode = "TRANSPARENT"
+    bpy.context.scene.render.film_transparent = True
+    bpy.context.scene.render.image_settings.color_mode = 'RGBA'
     bpy.context.scene.render.resolution_x = RESOLUTION[0]
     bpy.context.scene.render.resolution_y = RESOLUTION[1]
     bpy.context.scene.render.resolution_percentage = 100
